15bd39637d2dc3ebd994f74eb6436d8b373947a8
J.E. Remy <jeanerolle97@gmail.com>
2026-08-30 19:46:29 +0200

    Add files via upload

--- __init__.py
+++ __init__.py
@@ -1,1 +1,12 @@
+from .bootstrap import *
+from .version import *
 
+
+from .bootstrap import __all__ as _bootstrap_exports
+from .version import __all__ as _version_exports
+
+
+__all__ = [
+    *_bootstrap_exports,
+    *_version_exports,
+] # type: ignore
