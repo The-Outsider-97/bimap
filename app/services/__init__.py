66e1a99445382eb3a237922e03f2edfd9943d665
J.E. Remy <jeanerolle97@gmail.com>
2026-09-10 23:06:43 +0200

    Update __init__.py

--- app/services/__init__.py
+++ app/services/__init__.py
@@ -1,6 +1,8 @@
 from .account_service import *
+from .audit_input_service import *
 from .audit_service import *
 from .authentication_service import *
+from .data_extraction_service import *
 from .entitlement_service import *
 from .fulfilment_service import *
 from .model_conversion_service import *
@@ -9,8 +11,10 @@ from .review_service import *
 from .upload_service import *
 
 from .account_service import __all__ as _account_service_exports
+from .audit_input_service import __all__ as _audit_input_service_exports
 from .audit_service import __all__ as _audit_service_exports
 from .authentication_service import __all__ as _authentication_service_exports
+from .data_extraction_service import __all__ as _data_extraction_service_exports
 from .entitlement_service import __all__ as _entitlement_service_exports
 from .fulfilment_service import __all__ as _fulfilment_service_exports
 from .model_conversion_service import __all__ as _model_conversion_service_exports
@@ -20,8 +24,10 @@ from .upload_service import __all__ as _upload_service_exports
 
 __all__ = [
     *_account_service_exports,
+    *_audit_input_service_exports,
     *_audit_service_exports,
     *_authentication_service_exports,
+    *_data_extraction_service_exports,
     *_entitlement_service_exports,
     *_fulfilment_service_exports,
     *_model_conversion_service_exports,
